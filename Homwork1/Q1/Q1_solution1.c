5a9e58903224b0e6dc5c927675e4829f2b4f36ac
Ruiling Wang <43671776+wangruiling888@users.noreply.github.com>
2020-06-18 00:42:03 +1000

    Create Q1_solution1.c

---
+++ Homwork1/Q1/Q1_solution1.c
@@ -0,0 +1,39 @@
+// edited by Ruiling Wang
+# include "BST.h"
+void find_number(int * nums, int count){
+	assert(count > 2);
+	TreeNode t;
+	int sum = 0;
+	for (int i = 0; i < count -1; i++){
+		for (int j = i+1; j < count; j++){
+			//printf("%d %d %d %d\n", nums[i], nums[j], i ,j);
+			int a = nums[i], b = nums[j];
+			sum = a * a + b*b;
+			//printf("this is sum %d\n", sum);
+			if (i == 0 && j == 1) {
+				t = newNode(sum);
+			}
+			else if (SearchNode(t, sum) == 1) printf("%d\n", sum);
+			else{
+				InsertNode(t, sum);
+			}
+		}
+	}
+	return ;
+	
+}
+int main(){
+	printf("please enter a series of positive number:\n");
+	int num;
+	int * nums = malloc(sizeof(int));
+	int count = 0;
+	while (scanf("%d", &num) != EOF){
+		nums = realloc(nums, sizeof(count + 1) * sizeof(int));
+		nums[count] = num;
+		count ++;
+	} 
+	
+	find_number(nums, count);
+	printf("complete\n");
+	return 0;
+}
